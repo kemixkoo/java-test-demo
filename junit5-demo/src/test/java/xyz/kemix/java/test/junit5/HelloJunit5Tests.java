790c8e6df3d4dcf3ae7d2ec3bae022e9ef4bfafe
Kemix Koo <kemix_koo@163.com>
2018-06-10 22:48:50 +0800

    Add JUnit5 test demo

---
+++ junit5-demo/src/test/java/xyz/kemix/java/test/junit5/HelloJunit5Tests.java
@@ -0,0 +1,62 @@
+package xyz.kemix.java.test.junit5;
+
+import static java.time.Duration.ofMillis;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.condition.OS.LINUX;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import xyz.kemix.java.test.basic.Hello;
+
+@DisplayName("Test JUnit5 for horses")
+public class HelloJunit5Tests {
+    @DisabledOnOs(LINUX)
+    @RepeatedTest(value = 5, name = "第{currentRepetition}轮{displayName}测试")
+    @DisplayName("♞♘")
+    public void helloHorse(RepetitionInfo repetitionInfo) {
+        assertEquals(5, repetitionInfo.getTotalRepetitions()); 
+        
+        final Hello hello = new Hello();
+        assertTimeout(ofMillis(1000), () -> { // test in 1s
+            assertAll("horses", () -> {
+                final String blackHorse = hello.sayHello("black horse");
+                assertNotNull(blackHorse);
+                assertAll("black horse", //
+                        () -> assertThat(blackHorse, startsWith("Hello")), //
+                        () -> assertThat(blackHorse, containsString("black")), //
+                        () -> assertThat(blackHorse, endsWith("horse")));
+            }, () -> {
+                final String whiteHorse = hello.sayHello("white horse");
+                assertNotNull(whiteHorse);
+                assertAll("white horse", //
+                        () -> assertThat(whiteHorse, startsWith("Hello")), //
+                        () -> assertThat(whiteHorse, containsString("white")), //
+                        () -> assertThat(whiteHorse, endsWith("horse")));
+            });
+        });
+    }
+
+    @ParameterizedTest(name = "第{index}次我想要{0}马")
+    @ValueSource(strings = { "赤兔", "的卢", "白龙" })
+    @DisplayName("宝马测试")
+    public void helloBestHorse(String name) {
+        final Hello hello = new Hello();
+        final String blackHorse = hello.sayHello(name + "马");
+        assertAll("宝马", //
+                () -> assertThat(blackHorse, startsWith("Hello")), //
+                () -> assertThat(blackHorse, containsString(name)));
+    }
+
+}
